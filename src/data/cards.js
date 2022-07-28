e0b4e0572dfd7783f9403bf1ed05f9fae06846b1
wilhelminatetteh <yemoleytetteh333@gmail.com>
2022-07-28 19:58:43 +0000

    dummy card data

---
+++ src/data/cards.js
@@ -0,0 +1,16 @@
+const cards = [
+	{
+		face_image_url:
+			'https://www.shape.com/thmb/x-w9gx4d20cskiubFLw4MishK_E=/1244x933/smart/filters:no_upscale():focal(621x0:623x2)/Twist-Out-1-b1562caa0ee74676b875bb552b3a0d37.jpg',
+		first_name: 'Jane',
+		membership_number: 'xxxxxx',
+		date_of_birth: 'xxxxxx',
+		sex: 'female',
+		last_name: 'Doe',
+		address: 'Someplace',
+		phone_number: 'xxxxxxxxx',
+		start_date: 'xxxxx',
+		end_date: 'xxxxx',
+	},
+];
+export default cards;
